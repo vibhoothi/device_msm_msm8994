d70321336c7d57fddece4d71adf2d0dd8208863d
vibhoothiiaanand <vibhoothiiaanand@gmail.com>
2017-12-14 12:12:19 +0000

    oneplus2:AndroidBoard.mk:Fix WiFi path

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -76,7 +76,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := wpa_supplicant_overlay.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_SRC_FILES    := wifi/wpa_supplicant_overlay.conf
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
 include $(BUILD_PREBUILT)
 
@@ -84,7 +84,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := p2p_supplicant_overlay.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_SRC_FILES    := wifi/p2p_supplicant_overlay.conf
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
 include $(BUILD_PREBUILT)
 
@@ -93,7 +93,7 @@ LOCAL_MODULE       := hostapd_default.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
-LOCAL_SRC_FILES    := hostapd.conf
+LOCAL_SRC_FILES    := wifi/hostapd.conf
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -101,7 +101,7 @@ LOCAL_MODULE       := hostapd.accept
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
-LOCAL_SRC_FILES    := hostapd.accept
+LOCAL_SRC_FILES    := wifi/hostapd.accept
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -109,7 +109,7 @@ LOCAL_MODULE       := hostapd.deny
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
-LOCAL_SRC_FILES    := hostapd.deny
+LOCAL_SRC_FILES    := wifi/hostapd.deny
 include $(BUILD_PREBUILT)
 
 # Create symbolic links
