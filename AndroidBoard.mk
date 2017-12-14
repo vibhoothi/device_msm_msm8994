d83ecd9922c511833ed9dc3d34497c25766c85af
vibhoothiiaanand <vibhoothiiaanand@gmail.com>
2017-12-14 14:57:38 +0000

    oneplus2:AndroidBoard.mk:Fix fstab.qcom path

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -67,7 +67,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_SRC_FILES    := rootdir/etc/fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
