dac56d9998c4fe228bd16ffdfd8690b4c3b29976
vibhoothiiaanand <vibhoothiiaanand@gmail.com>
2017-12-16 11:23:00 +0000

    oneplus2:BoardConfig:Add TARGET_OTA_ASSERT_DEVICE

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,7 @@ TARGET_NO_KERNEL := false
 TARGET_KERNEL_APPEND_DTB := true
 -include $(QCPATH)/common/msm8994/BoardConfigVendor.mk
 
+TARGET_OTA_ASSERT_DEVICE := OnePlus2,oneplus2
 
 # Some framework code requires this to enable BT
 BOARD_HAVE_BLUETOOTH := true
